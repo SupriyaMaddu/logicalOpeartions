4482e25f69c564115647e09ae8cb0c4c57f8a39f
Supriya Maddu <117802859+SupriyaMaddu@users.noreply.github.com>
2023-08-22 15:42:24 +0530

    Add files via upload

---
+++ logicalOperators.py
@@ -0,0 +1,33 @@
+#greater than 9 
+
+A = int(input())
+B = int(input())
+
+condition = A > 9 and B > 9 
+print(condition)
+
+#negative numbers 
+
+A = int(input())
+B = int(input())
+
+condition = A < 0 and B < 0 
+print(condition)
+
+#positive number 
+
+A = int(input())
+B = int(input())
+
+condition = A > 0 or B > 0 
+print(condition) 
+
+#sum is not greater than 100 
+
+A = int(input())
+B = int(input()) 
+
+sum_of_A_and_B = A + B 
+condition = sum_of_A_and_B <= 100 
+print(condition)
+
